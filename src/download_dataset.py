8211363f5a01a03ba7e13b240d20131e24d449ac
OJLafond <115372464+OJLafond@users.noreply.github.com>
2025-05-08 09:27:01 -0400

    Delete src/download_dataset.py

--- src/download_dataset.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def download_physionet_dataset(target_dir="data/physionet"):
-    os.makedirs(target_dir, exist_ok=True)
-    os.system(f"wget -r -N -c -np https://physionet.org/files/noneeg/1.0.0/ -P {target_dir}")
-
-if __name__ == "__main__":
-    download_physionet_dataset()

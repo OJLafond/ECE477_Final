89e17834ede38169c1a88846ad964d73a0f087cd
OJLafond <115372464+OJLafond@users.noreply.github.com>
2025-05-07 21:18:56 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,148 +3,185 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from torch.utils.data import DataLoader, TensorDataset
-import os
-import shutil
 
-# Helper function to create sparse masks
-def neuron_mask(mask, add_index, func='in', sparse=False, ratio=0.2):
-    if sparse:
-        num = int(mask.size(0) * ratio)
-        if func == 'in':
-            for i in add_index:
-                indices = np.random.permutation(mask.size(0))[:num]
-                mask[indices, i] = 1
-        elif func == 'out':
-            for i in add_index:
-                indices = np.random.permutation(mask.size(1))[:num]
-                mask[i, indices] = 1
-    else:
-        if func == 'in':
-            mask[:, add_index] = 1
-        elif func == 'out':
-            mask[add_index, :] = 1
-    return mask
-
-class SDNN(nn.Module):
+class SDNN:
     def __init__(self, in_num, out_num, init_size, max_size, batch_size, scheme='A'):
-        super(SDNN, self).__init__()
         self.in_num = in_num
         self.out_num = out_num
         self.init_size = init_size
         self.max_size = max_size
-        self.cur_size = init_size
+        self.cur_size = init_size - 1
         self.batch_size = batch_size
         self.scheme = scheme
+        self.name = 'SDNN'
         self.epoch = 0
         self.best_acc = 0
         self.best_acc_prune = 0
+        self.now_acc = 0
         self.flag = 0
-        self.active_index = list(range(init_size))
+        self.active_index = []
 
-        # Weight and bias parameters
-        self.w1 = nn.Parameter(torch.randn(in_num, max_size) * 0.1)
-        self.w2 = nn.Parameter(torch.randn(max_size, max_size) * 0.1)
-        self.w3 = nn.Parameter(torch.randn(max_size, out_num) * 0.1)
-        self.w4 = nn.Parameter(torch.randn(in_num, out_num) * 0.1)
-        self.b1 = nn.Parameter(torch.zeros(1, max_size))
-        self.b2 = nn.Parameter(torch.zeros(1, out_num))
+    def neuronMask(self, mask, add_index, func='in', sparse=False, ratio=0.2):
+        if sparse:
+            num = int(mask.size(0) * ratio)
+            for i in add_index:
+                indices = list(range(mask.size(0)))
+                np.random.shuffle(indices)
+                selected = indices[:num]
+                if func == 'in':
+                    mask[selected, i] = 1
+                elif func == 'out':
+                    mask[i, selected] = 1
+        else:
+            for i in add_index:
+                if func == 'in':
+                    mask[:, i] = 1
+                elif func == 'out':
+                    mask[i, :] = 1
+        return mask
 
-        # Masks
-        self.m1 = torch.zeros(in_num, max_size)
-        self.m2 = torch.ones(max_size, max_size)
-        self.m3 = torch.zeros(max_size, out_num)
-        self.m4 = torch.ones(in_num, out_num)
+    def structureInit(self, sparse=True, ratio=0.2):
+        self.w1 = torch.randn(self.in_num, self.max_size) * 0.1
+        self.m1 = torch.zeros(self.in_num, self.max_size)
+        self.w2 = torch.randn(self.max_size, self.max_size) * 0.1
+        self.m2 = torch.ones(self.max_size, self.max_size)
+        self.w3 = torch.randn(self.max_size, self.out_num) * 0.1
+        self.m3 = torch.zeros(self.max_size, self.out_num)
+        self.w4 = torch.randn(self.in_num, self.out_num) * 0.1
+        self.m4 = torch.ones(self.in_num, self.out_num)
+        self.b1 = torch.zeros(1, self.max_size)
+        self.b2 = torch.zeros(1, self.out_num)
 
-        # Loss and optimizer
+        for p in [self.w1, self.w2, self.w3, self.w4, self.b1, self.b2]:
+            p.requires_grad = True
+
+        self.params = {'w1': self.w1, 'w2': self.w2, 'w3': self.w3, 'w4': self.w4,
+                       'm1': self.m1, 'm2': self.m2, 'm3': self.m3, 'm4': self.m4}
+
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD([self.w1, self.w2, self.w3, self.w4, self.b1, self.b2],
                                    lr=0.001, momentum=0.9, weight_decay=1e-4, nesterov=True)
 
-        # Initialize structure
-        self._initialize_structure()
-
-    def _initialize_structure(self, sparse=True, ratio=0.2):
         self.active_index = list(range(self.init_size))
-        if sparse:
-            self.m1 = neuron_mask(self.m1, self.active_index, sparse=True, ratio=ratio)
-        else:
-            self.m1 = neuron_mask(self.m1, self.active_index)
-        self.m3 = neuron_mask(self.m3, self.active_index, func='out')
-        self.b1.data = neuron_mask(self.b1.data, self.active_index)
+        self.m1.data = self.neuronMask(self.m1.data, self.active_index, sparse=sparse, ratio=ratio)
+        self.m3.data = self.neuronMask(self.m3.data, self.active_index, func='out')
+        self.b1.data = self.neuronMask(self.b1.data, self.active_index)
 
-    def forward(self, x):
-        hidden = torch.zeros(x.size(0), self.max_size, device=x.device)
+    def forward(self, x, retain_grad=True):
+        self.hidden = torch.zeros(x.size(0), self.max_size)
         for j in self.active_index:
-            x_proj = torch.mm(x, self.w1[:, j] * self.m1[:, j].to(x.device))
-            h_proj = torch.mm(hidden, self.w2[:, j] * self.m2[:, j].to(x.device))
-            hidden[:, j] = F.relu(x_proj + h_proj + self.b1[:, j])
-
-        out = torch.mm(hidden, self.w3 * self.m3.to(x.device)) \
-            + torch.mm(x, self.w4 * self.m4.to(x.device)) + self.b2
+            self.hidden[:, j] = F.relu(
+                torch.mm(self.hidden.clone(), self.w2[:, j] * self.m2[:, j].view(-1, 1)) +
+                torch.mm(x, self.w1[:, j] * self.m1[:, j].view(-1, 1)) +
+                self.b1[:, j]
+            ).squeeze(1)
+        out = torch.mm(self.hidden, self.w3 * self.m3) + torch.mm(x, self.w4 * self.m4) + self.b2
+        if retain_grad:
+            out.retain_grad()
         return out
 
-    def load_data(self, X_train, y_train, X_test, y_test):
-        self.X_train = torch.tensor(X_train, dtype=torch.float32)
-        self.y_train = torch.tensor(y_train, dtype=torch.long)
-        self.X_test = torch.tensor(X_test, dtype=torch.float32)
-        self.y_test = torch.tensor(y_test, dtype=torch.long)
+    def apply_masks(self):
+        self.w1.data *= self.m1.data
+        self.w2.data *= self.m2.data
+        self.w3.data *= self.m3.data
+        self.w4.data *= self.m4.data
 
-        self.trainloader = DataLoader(TensorDataset(self.X_train, self.y_train), batch_size=self.batch_size, shuffle=True)
-        self.testloader = DataLoader(TensorDataset(self.X_test, self.y_test), batch_size=self.batch_size, shuffle=False)
+    def backwardGrad(self, outgrad):
+        self.hidden.grad = torch.mm(outgrad, self.w3.T)
+        rev_idx = list(reversed(self.active_index))
+        for i, j in enumerate(rev_idx):
+            for k in range(i):
+                self.hidden.grad[:, j] += self.hidden.grad[:, k] * self.w2[j, k]
 
-    def train_model(self, num_epochs):
-        for epoch in range(num_epochs):
-            self.train()
-            running_loss = 0.0
-            for inputs, labels in self.trainloader:
+    def addConnection(self, percentile={'m1': 70, 'm2': 70, 'm3': 70, 'm4': 70}):
+        loader = iter(self.trainloader)
+        inputs, labels = next(loader)
+        self.optimizer.zero_grad()
+        outputs = self.forward(inputs)
+        loss = self.criterion(outputs, labels)
+        loss.backward()
+        self.backwardGrad(outputs)
+
+        cov_mat = {
+            'm1': torch.mm(inputs.T, self.hidden.grad).detach().numpy(),
+            'm2': torch.mm(self.hidden.T, self.hidden.grad).detach().numpy(),
+            'm3': torch.mm(self.hidden.T, outputs.grad).detach().numpy(),
+            'm4': torch.mm(inputs.T, outputs.grad).detach().numpy()
+        }
+
+        for key in percentile:
+            threshold = np.percentile(cov_mat[key][np.nonzero(cov_mat[key])], percentile[key])
+            self.params[key].data[torch.tensor(cov_mat[key]) > threshold] = 1
+
+        self.apply_masks()
+
+    def pruneConnections(self, prune_ratio=0.2):
+        for key in ['m1', 'm2', 'm3', 'm4']:
+            weight = self.params[key.replace('m', 'w')]
+            mask = self.params[key]
+            active_weights = torch.abs(weight.data[mask.data == 1])
+            if active_weights.numel() == 0:
+                continue
+            threshold = torch.quantile(active_weights, prune_ratio)
+            to_prune = (torch.abs(weight.data) < threshold) & (mask.data == 1)
+            mask.data[to_prune] = 0
+            weight.data *= mask.data
+
+    def cellDivision(self, num=1):
+        activation = torch.sum(torch.abs(self.hidden), 0).detach().numpy()
+        max_index_arr = np.flip(np.argsort(activation)[-num:], axis=0)
+
+        for max_index in max_index_arr:
+            add_index = len(self.active_index)
+            if add_index >= self.max_size:
+                continue
+            if max_index not in self.active_index:
+                continue
+
+            self.active_index.insert(self.active_index.index(max_index), add_index)
+            self.m1[:, add_index] = self.m1[:, max_index]
+            self.m2[:, add_index] = self.m2[:, max_index]
+            self.m3[add_index, :] = self.m3[max_index, :]
+
+            self.w1[:, add_index] = self.w1[:, max_index] + torch.randn(self.in_num) * 0.01
+            self.w2[:, add_index] = self.w2[:, max_index] + torch.randn(self.max_size) * 0.01
+            self.w3[add_index, :] = self.w3[max_index, :] + torch.randn(self.out_num) * 0.01
+            self.b1[:, add_index] = self.b1[:, max_index]
+
+        self.apply_masks()
+
+    def train(self, duration=10):
+        for epoch in range(self.epoch, self.epoch + duration):
+            self.epoch = epoch
+            for i, (inputs, labels) in enumerate(self.trainloader):
+                inputs.requires_grad_(True)
                 self.optimizer.zero_grad()
                 outputs = self.forward(inputs)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
+                self.apply_masks()
 
-                # Apply masking
-                self.w1.data *= self.m1
-                self.w2.data *= self.m2
-                self.w3.data *= self.m3
-                self.w4.data *= self.m4
+    def loadData(self, X_train, y_train, X_test, y_test):
+        self.X_train = torch.tensor(X_train, dtype=torch.float32)
+        self.y_train = torch.tensor(y_train.reshape(-1), dtype=torch.long)
+        self.X_test = torch.tensor(X_test, dtype=torch.float32)
+        self.y_test = torch.tensor(y_test.reshape(-1), dtype=torch.long)
 
-                running_loss += loss.item()
-            print(f"Epoch {epoch+1}, Loss: {running_loss:.4f}")
+        self.trainloader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(self.X_train, self.y_train),
+            batch_size=self.batch_size, shuffle=True)
+
+        self.testloader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(self.X_test, self.y_test),
+            batch_size=self.batch_size, shuffle=False)
 
     def evaluate(self):
-        self.eval()
-        correct, total = 0, 0
+        correct = total = 0
         with torch.no_grad():
-            for x, y in self.testloader:
-                outputs = self.forward(x)
-                preds = torch.argmax(outputs, dim=1)
-                correct += (preds == y).sum().item()
-                total += y.size(0)
-        acc = correct / total
-        print(f"Test Accuracy: {acc:.4f}")
-        return acc
-
-    def save_checkpoint(self, path, is_best=False):
-        state = {
-            'epoch': self.epoch,
-            'best_acc': self.best_acc,
-            'active_index': self.active_index,
-            'state_dict': {
-                'w1': self.w1.data,
-                'w2': self.w2.data,
-                'w3': self.w3.data,
-                'w4': self.w4.data,
-                'b1': self.b1.data,
-                'b2': self.b2.data,
-                'm1': self.m1,
-                'm2': self.m2,
-                'm3': self.m3,
-                'm4': self.m4,
-            },
-            'optimizer': self.optimizer.state_dict()
-        }
-        torch.save(state, path)
-        if is_best:
-            shutil.copy(path, path.replace('.pth', '_best.pth'))
+            for inputs, labels in self.testloader:
+                outputs = self.forward(inputs, retain_grad=False)
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
+        return correct / total if total > 0 else 0
